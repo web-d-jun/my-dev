51dfd796241fdce204b79f198c37cb00b234db36
junyoung <cjy874545@gmail.com>
2019-12-03 13:19:52 +0900

    Revert "pwa add"
    This reverts commit e81bef5f63e7f69e635f5b8a4f7a2911ce0cd9b7.

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  pwa: {
-    name: 'my dev',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
+  ]
 }
