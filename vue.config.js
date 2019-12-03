079f40ef3b7cc7a73cbbd50d7c2a99200bae3540
junyoung <cjy874545@gmail.com>
2019-12-03 18:16:08 +0900

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'My Dev',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'public/service-worker.js'
+      // ...other Workbox options...
+    }
+  }
 }
