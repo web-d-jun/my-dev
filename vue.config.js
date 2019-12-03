ca17e1856ae8863bbf883a78a098c8602ba50a3a
junyoung <cjy874545@gmail.com>
2019-12-03 19:08:18 +0900

    typescript add

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,15 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  pwa: {
-    name: 'My Dev',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'public/service-worker.js'
-      // ...other Workbox options...
+  "pwa": {
+    "name": "My Dev",
+    "themeColor": "#4DBA87",
+    "msTileColor": "#000000",
+    "appleMobileWebAppCapable": "yes",
+    "appleMobileWebAppStatusBarStyle": "black",
+    "workboxPluginMode": "InjectManifest",
+    "workboxOptions": {
+      "swSrc": "public/service-worker.js"
     }
   }
 }
