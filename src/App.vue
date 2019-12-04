3a79a7776d50a27bb2383969a01af0134cce06a8
junyoung <cjy874545@gmail.com>
2019-12-04 09:09:40 +0900

    layout add

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,76 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.text" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        
+        <v-list-item class="mt-4" link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left color="red" dense>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-icon class="mx-4">fab fa-youtube</v-icon>
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="title">룰루랄라</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-row align="center" style="max-width: 650px">
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="mdi-magnify"
+          color="white"
+          hide-details
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-row>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container class="fill-height">
+       <v-col>
+         
+       </v-col>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue, Prop } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-});
+@Component
+export default class App extends Vue {
+  @Prop() source: any;
+  theme: any = { dark: true };
+  drawer: any = null;
+  items: any = [
+    { icon: "mdi-anchor", text: "Most Popular" },
+    { icon: "mdi-account", text: "게시판" }
+  ];
+  
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex';
+import store from './store';
+import router from './router';
 import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
-Vue.use(Vuex);
 
 new Vue({
+  router,
+  store,
   vuetify,
   render: h => h(App),
 }).$mount('#app')
