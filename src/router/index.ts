ee01183cfbb25f5d1e0c370b77cf8db29cee10fc
junyounga <cjy874545@gmail.com>
2020-01-10 02:51:13 +0900

    router modi

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,49 +1,44 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home,
+    path: "/",
+    name: "home",
+    component: () => import("@/views/Home.vue")
   },
   {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: () => import('@/views/notice_board.vue')
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("@/views/notice_board.vue")
   },
   {
-    path: '/vue/notice_board',
-    name: 'notice_board',
-    component: () => import('@/views/notice_board.vue'),
-  },
+    path: "/vue/notice_board",
+    name: "notice_board",
+    component: () => import("@/views/notice_board.vue")
+  }
   // {
   //   path: '/notice_board',
   //   name: 'notice_board',
   //   component: () => import('@/views/notice_board.vue')
   // }
-
-
-
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.path === '/') {
+  if (to.path === "/") {
     next();
   }
   if (to.path !== from.path) {
     next();
   }
-
-})
+});
 
 export default router;
