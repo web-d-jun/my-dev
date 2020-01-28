4a72203de7d3015d0242c9c78d811aaedd525c81
junyounga <cjy874545@gmail.com>
2020-01-28 14:11:18 +0900

    custom-wrap edit

--- src/components/common/custom-wrap/index.vue
+++ src/components/common/custom-wrap/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="custom-wrap">
-    <slot></slot>
+    <div class="custom-content">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -13,7 +15,16 @@ export default class CustomWrap extends Vue {}
 <style lang="scss">
 .custom-wrap {
   width: 100%;
-  padding: 20px;
-  background-color: rgba($color: #fff, $alpha: 0.3);
+  padding: 15px;
+
+  .custom-content {
+    width: 100%;
+    background-color: rgba($color: #fff, $alpha: 0.3);
+    padding: 15px;
+    border-radius: 10px;
+    box-shadow: 0px 5px 6px -3px rgba($color: #000, $alpha: 0.2),
+      0px 9px 12px 1px rgba($color: #000, $alpha: 0.14),
+      0px 3px 16px 2px rgba($color: #000, $alpha: 0.2);
+  }
 }
 </style>
