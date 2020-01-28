385762e771d27c94568aaaa5a238b9e921d2eec9
junyounga <cjy874545@gmail.com>
2020-01-28 16:44:26 +0900

    title component add

---
+++ src/components/j-title/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="title-wrap">
+    <div class="title-container">{{title}}</div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class jTitle extends Vue {
+  @Prop({ default: "-" }) readonly title!: string;
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
