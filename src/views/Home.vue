656083b060a7136b93581c5a5296a9032ff6ec3c
junyounga <cjy874545@gmail.com>
2020-02-04 02:34:53 +0900

    home update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,75 @@
 <template>
-  <div class="home">
+  <div class="home__dashboard">
     <is-loader></is-loader>
-    <div>Home</div>
+    <v-row>
+      <v-flex md3 sm12 xs12>
+        <custom-wrap>
+          <div class="card__wrap">
+            <v-card>
+              <div class="d-flex flex-column">
+                <div class="d-flex flex-row align-center">Total data1-1</div>
+                <div class="d-flex flex-row justify-space-between align-center">
+                  <bar-graph :width="'100px'" :height="'60px'" :data="data1" :border="'green'"></bar-graph>
+                  <j-badge :innerText="'20'"></j-badge>
+                </div>
+              </div>
+            </v-card>
+          </div>
+        </custom-wrap>
+      </v-flex>
+      <v-flex md3 sm12 xs12>
+        <custom-wrap>
+          <div class="card__wrap">
+            <v-card>
+              <div class="d-flex flex-column">
+                <div class="d-flex flex-row align-center">Total data1-1</div>
+                <div class="d-flex flex-row justify-space-between align-center">
+                  <bar-graph :width="'100px'" :height="'60px'" :data="data2" :border="'purple'"></bar-graph>
+                  <j-badge :innerText="'20'"></j-badge>
+                </div>
+              </div>
+            </v-card>
+          </div>
+        </custom-wrap>
+      </v-flex>
+      <v-flex md3 sm12 xs12>
+        <custom-wrap>
+          <div class="card__wrap">
+            <v-card>3</v-card>
+          </div>
+        </custom-wrap>
+      </v-flex>
+      <v-flex md3 sm12 xs12>
+        <custom-wrap>
+          <div class="card__wrap">
+            <v-card>4</v-card>
+          </div>
+        </custom-wrap>
+      </v-flex>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
-import Vue from "vue";
+import { Vue, Component, Emit } from "vue-property-decorator";
 import isLoader from "@/components/common/loader/index.vue";
-import { Component } from "vue-property-decorator";
+import CustomWrap from "@/components/common/custom-wrap/index.vue";
+import jBadge from "@/components/badge/index.vue";
+import barGraph from "@/components/graph/bar.vue";
 import { Mutation, namespace } from "vuex-class";
 
 @Component({
   components: {
-    isLoader
+    isLoader,
+    CustomWrap,
+    jBadge,
+    barGraph
   }
 })
 export default class home extends Vue {
+  data1: object[] = [];
+  data2: object[] = [];
   @Mutation("loaderInit", { namespace: "loaderModule" })
   loaderInit!: any;
   @Mutation("loaderDisplayShow", { namespace: "loaderModule" })
@@ -26,16 +78,53 @@ export default class home extends Vue {
   created() {
     this.loaderInit();
     this.getData();
+    this.getChart();
   }
 
   async getData() {
     try {
+      await setInterval(this.getChart, 5000);
       setTimeout(() => {
         this.loaderDisplayShow();
-      }, 5000);
+      }, 1000);
     } catch (e) {
       console.error(e.message);
     }
   }
+
+  @Emit()
+  getChart() {
+    this.data1 = [];
+    this.data2 = [];
+    for (let i = 0; i < 6; i++) {
+      this.data1.push({
+        value: Math.round(Math.random() * 100),
+        background: "green"
+      });
+      this.data2.push({
+        value: Math.round(Math.random() * 100),
+        background: "purple"
+      });
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+.home__dashboard {
+  width: 100%;
+  height: 100%;
+
+  .card__wrap {
+    width: 100%;
+    height: 120px;
+    .v-card {
+      width: 100%;
+      height: 100%;
+      padding: 10px;
+      .flex-column {
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
