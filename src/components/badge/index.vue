9f303d91c2ee45cfe2371dcdb2fb93e929fbd969
junyounga <cjy874545@gmail.com>
2020-02-04 02:34:42 +0900

    badge component add

---
+++ src/components/badge/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="badge">{{innerText}}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class jBadge extends Vue {
+  @Prop() innerText!: string;
+}
+</script>
+<style lang="scss" scoped>
+#badge {
+}
+</style>
