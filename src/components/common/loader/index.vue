02cf5ec9866d115b6b0ae33af7bec666f49dad82
junyounga <cjy874545@gmail.com>
2020-02-12 02:55:41 +0900

    loader css 높이 수정

--- src/components/common/loader/index.vue
+++ src/components/common/loader/index.vue
@@ -50,7 +50,7 @@ export default class isLoader extends Vue {
 <style lang="scss" scoped>
 .is-loading-container {
   width: 100%;
-  height: 600px;
+  height: 700px;
   position: absolute;
   left: 0;
   top: 0;
