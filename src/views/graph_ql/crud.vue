dfe023e666db58161b23be046cf99ae1f034023d
junyounga <cjy874545@gmail.com>
2020-02-11 21:05:16 +0900

    graphql crud add

---
+++ src/views/graph_ql/crud.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>zzzz</div>
+</template>
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class graphqlCrudPage extends Vue {}
+</script>
+<style lang="scss">
+</style>
