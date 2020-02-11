92a7be10b4494476507d52c836ef1d58a82808aa
junyounga <cjy874545@gmail.com>
2020-02-11 21:05:00 +0900

    404.vue add

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>404</div>
+  <div class="error-page__container d-flex">
+    <div class="inner__text">
+      <strong>404</strong>
+    </div>
+  </div>
 </template>
 
 
@@ -9,3 +13,16 @@ import { Vue } from "vue-property-decorator";
 
 export default class __404__ extends Vue {}
 </script>
+<style lang="scss">
+.error-page__container {
+  width: 100%;
+  height: 100%;
+  
+  align-items: center;
+  justify-content: center;
+  .inner__text {
+    font-size: 90pt;
+    color: #424242;
+  }
+}
+</style>
