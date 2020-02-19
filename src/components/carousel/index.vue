807d352591912cb89a543b4c396e04bacee70f5e
junyounga <cjy874545@gmail.com>
2020-02-19 22:50:29 +0900

    scroll 수정

--- src/components/carousel/index.vue
+++ src/components/carousel/index.vue
@@ -16,16 +16,27 @@ export default class JCarousel extends Vue {}
 <style lang="scss" scoped>
 .carousel__container__wrap {
   overflow: hidden;
+  height: 33px;
+  border-bottom: 1px solid #d8dce5;
   .carousel__container {
     width: 100%;
     overflow-x: auto;
     overflow-y: hidden;
-    border-bottom: 1px solid #d8dce5;
+
+    &:hover {
+      &::-webkit-scrollbar {
+        width: 5px;
+        height: 8px;
+        background-color: transparent;
+        display: block !important;
+      }
+    }
 
     &::-webkit-scrollbar {
       width: 5px;
       height: 8px;
       background-color: transparent;
+      display: none;
     }
     &::-webkit-scrollbar-thumb {
       background: rgba(255, 255, 255, 0.5);
